510356b3b4f1ae0fb61ef79459fc8f7ddfa5c917
maeveBestDev <77807261+maeveBestDev@users.noreply.github.com>
2021-09-22 20:20:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-
+function controle(){
+var saisie =document.getElementById("input").value;
+if (saisie == "home"){
+  document.location.href="https://maevebestdev.github.io/Main_Page/";
+  }
+if (saisie == "videos games"){
+    document.location.href="https://maevebestdev.github.io/MesJeux/";
+  }
+if(saisie == "bot"){
+  document.location.href="https://maevebestdev.github.io/MesBots/";
+}
+if(saisie == "website"){
+  document.location.href="https://maevebestdev.github.io/MesSite/";
+}
+if(saisie == "about"){
+  document.location.href="https://maevebestdev.github.io/About_Us/";
+}
+if(saisie == "help"){
+  document.location.href="https://maevebestdev.github.io/Help/";
+}
+if(saisie == "contact"){
+  document.location.href="https://maevebestdev.github.io/Contact_Us/";
+}
+}
